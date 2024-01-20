b58f3f0176116bfa4ff21ed03595d0f0d01529ba
Ben Jackson <Benjamin.Jackson.2022@live.rhul.ac.uk>
2024-01-20 20:56:06 +0000

    Added Greetings using jsx, and added a dark/light mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tutorial-blog23.netlify.app/"
+  site: "https://astro-tutorial-blog23.netlify.app/",
+  integrations: [preact()]
 });
